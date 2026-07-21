11881c59617db9c848a9be39068f22e67a2d6cdf
Adhiii <adithyanshj@gmail.com>
2026-07-22 00:40:20 +0530

    ✅ Day 1: Data Types [python3] — approach1

---
+++ solutions/python3/easy/30-data-types/solution-approach1.py
@@ -0,0 +1,22 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/30-data-types/problem?isFullScreen=true
+# Problem     Day 1: Data Types
+# Difficulty  Easy
+# Subdomain   30 Days of Code
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-22, 12:40 a.m.
+# ──────────────────────────────────────────────────
+
+
+
+
+my_int = int(input())
+my_double = float(input())
+my_string = input()
+
+print(i + my_int)
+print(d + my_double)
+print(s + my_string)
+
