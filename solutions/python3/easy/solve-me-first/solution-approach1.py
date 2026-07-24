be80934f3e75400561bf74fd41b39c9f9b87755c
Adhiii <adithyanshj@gmail.com>
2026-07-24 17:03:44 +0530

    ✅ Solve Me First [python3] — approach1

---
+++ solutions/python3/easy/solve-me-first/solution-approach1.py
@@ -0,0 +1,22 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+# Problem     Solve Me First
+# Difficulty  Easy
+# Subdomain   Warmup
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-24, 05:03 p.m.
+# ──────────────────────────────────────────────────
+
+
+
+def solveMeFirst(a,b):
+    sum = a + b
+    
+    return sum
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
